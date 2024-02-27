c3aee2aad5f25ca9c4398a7e5d5cbcbd8a69f3d2
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-02-26 21:42:13 -0300

    Criando interceptador responsável por enviar parâmetros e o token para a requisição

---
+++ app/src/main/java/br/com/movieapp/core/data/remote/ParamsInterceptor.kt
@@ -0,0 +1,24 @@
+package br.com.movieapp.core.data.remote
+
+import br.com.movieapp.BuildConfig
+import br.com.movieapp.core.util.Constants
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class ParamsInterceptor : Interceptor {
+
+    //Método responsável por interceptar as requisições antes que elas sejam enviadas para o servidor (API)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request() //Obtendo requisição original
+        val url = request.url.newBuilder()
+            .addQueryParameter(Constants.LANGUAGE_PARAM, Constants.LANGUAGE_VALUE)
+            .addQueryParameter(Constants.API_TOKEN_PARAM, BuildConfig.JWT_TOKEN)
+            .build()
+
+        val newRequest = request.newBuilder()
+            .url(url)
+            .build()
+
+        return chain.proceed(newRequest)
+    }
+}
---
+++ app/src/main/java/br/com/movieapp/core/util/Constants.kt
@@ -0,0 +1,8 @@
+package br.com.movieapp.core.util
+
+object Constants {
+
+    const val API_TOKEN_PARAM = "Authorization"
+    const val LANGUAGE_PARAM = "language"
+    const val LANGUAGE_VALUE = "pt-BR"
+}
