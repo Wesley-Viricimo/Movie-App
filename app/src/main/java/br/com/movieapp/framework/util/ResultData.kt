ad9c6005015260ae12c82c4a67f7066123a53599
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-02-27 22:06:03 -0300

    Criando classes utilitarias

---
+++ app/src/main/java/br/com/movieapp/framework/util/ResultData.kt
@@ -0,0 +1,8 @@
+package br.com.movieapp.framework.util
+
+sealed class ResultData<out T> { //Classe irá representar os diferentes estados das operações que serão realizadas
+
+    object Loading : ResultData<Nothing>()
+    data class Success<out T>(val data: T?) : ResultData<T>()
+    data class Failure(val e : Exception?) : ResultData<Nothing>()
+}
