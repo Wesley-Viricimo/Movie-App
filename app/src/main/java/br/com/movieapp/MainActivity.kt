3f8d5e8798491b360ff338184870c91888b0d535
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-04-05 21:30:59 -0300

    Implementando splash screen

--- app/src/main/java/br/com/movieapp/MainActivity.kt
+++ app/src/main/java/br/com/movieapp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import br.com.movieapp.core.presentation.MainScreen
 import br.com.movieapp.ui.theme.MovieAppTheme
@@ -23,6 +24,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             MovieAppTheme {
                 MainScreen(navController = rememberNavController())
